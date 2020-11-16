b94cef71cd3ded02b838e5b41fc202307cba1a77
sumitsamsharma <sharmasumit94286@gmail.com>
2020-11-16 21:34:37 +0530

    Finding cheapest hotel by date range

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -17,11 +17,12 @@ public class HotelReservationTest {
     }
 
     @Test
-    public void givenHotelList_ShouldReturnCheapestHotel() {
-        hotelReservationMain.hotelList.add(new Hotel("ITC", 10000));
-        hotelReservationMain.hotelList.add(new Hotel("Taj", 6000));
-        hotelReservationMain.hotelList.add(new Hotel("Mahindra", 9000));
-        String cheapestHotel = hotelReservationMain.findCheapestHotel();
-        Assert.assertEquals("Taj", cheapestHotel);
+    public void givenHotelList_ShouldReturnCheapestHotelInGivenDateRange()
+    {
+        hotelReservationMain.hotelList.add(new Hotel("ITC", 10000, "2020-01-25", "2020-01-26"));
+        hotelReservationMain.hotelList.add(new Hotel("Taj", 6000, "2020-02-12", "2020-02-14"));
+        hotelReservationMain.hotelList.add(new Hotel("Mahindra", 9000, "2020-11-01", "2020-11-04"));
+        Hotel cheapestHotel = hotelReservationMain.findCheapestHotel("2020-01-01", "2020-12-31");
+        Assert.assertEquals("Taj", cheapestHotel.hotelName);
     }
 }
